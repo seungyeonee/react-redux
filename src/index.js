68019d24ef785aa68ca09b9fc890c771e7f25738
Sylee <seungyeoni.dev@gmail.com>
2022-11-16 12:41:36 +0900

    rootReducer, store 생성 및 Redux DevTools 세팅

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { legacy_createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import rootReducer from './modules';
+
+const store = legacy_createStore(
+  rootReducer, /* preloadedState, */
+  composeWithDevTools() /*window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() */
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
